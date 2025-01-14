35294efd39c65e378439d42296af5b7ea2252572
Wany917 <yvannkakou03@gmail.com>
2025-01-14 09:30:53 +0100

    asm 05 V1

--- asm05/asm05.s
+++ asm05/asm05.s
@@ -1,28 +1,34 @@
 section .text
-global _start
+    global _start
 
 _start:
-    ; Get argc from stack
     pop rax         ; Get argc
-    pop rax         ; Skip argv[0] (program name)
-    pop rdi         ; Get argv[1] (first argument)
+    cmp rax, 2      ; Check if we have at least one argument
+    jl error        ; If not, jump to error
+
+    pop rax         ; Skip argv[0]
+    pop rdi         ; Get argv[1]
 
     ; Calculate string length
-    mov rcx, -1     ; Initialize counter
-    mov al, 0       ; Search for null terminator
-    cld             ; Clear direction flag
-    repne scasb     ; Scan string
-    not rcx         ; Invert counter
-    dec rcx         ; Exclude null terminator
-    
-    ; Print the string
-    mov rax, 1      ; sys_write
+    mov rcx, -1     ; Initialize counter to -1
+    xor al, al      ; al = 0 (searching for NULL)
+    mov rsi, rdi    ; Copy string address
+    repne scasb     ; Scan until NULL
+    neg rcx         ; Get actual length
+    dec rcx         ; Subtract 1 to exclude NULL
+
+    ; Write to stdout
+    mov rax, 1      ; syscall write
     mov rdi, 1      ; stdout
-    mov rsi, rdi    ; Use the pointer we got from pop rdi earlier
-    ; rcx already contains length
+    mov rsi, [rsp-8]; Get back argv[1]
+    mov rdx, rcx    ; length
     syscall
 
-    ; Exit program
-    mov rax, 60     ; sys_exit
+    mov rax, 60     ; syscall exit
     xor rdi, rdi    ; return 0
+    syscall
+
+error:
+    mov rax, 60     ; syscall exit
+    mov rdi, 1      ; return 1
     syscall
