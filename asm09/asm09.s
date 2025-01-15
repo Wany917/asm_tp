c44b01f843954c20ece82e1d540ad136bf35b82a
Wany917 <yvannkakou03@gmail.com>
2025-01-15 10:24:58 +0100

    asm 9 10 done

--- asm09/asm09.s
+++ asm09/asm09.s
@@ -1,75 +1,44 @@
-; asm_convert.s
-; ---------------------------------------------
-; Usage :
-;   ./asm_convert 255
-;       => Affiche la conversion hexa de 255 (FF)
-;
-;   ./asm_convert -b 42
-;       => Affiche la conversion binaire de 42 (101010)
-; ---------------------------------------------
-
 section .text
 global _start
 
 _start:
-    ; Récupération de argc dans rax
-    mov rax, [rsp]        ; rax = argc
+    mov rax, [rsp]
 
-    ; On s’attend à 2 arguments => standard_convert
-    ; ou 3 arguments => binary_convert
     cmp rax, 2
     je standard_convert
     cmp rax, 3
     je binary_convert
     jmp error_param
 
-; -------------------------------------------------------------------
-; standard_convert: 
-;   ./asm_convert 255
-;   => convertit 255 (argv[1]) en hexadécimal
-; -------------------------------------------------------------------
 standard_convert:
-    ; argv[1] = [rsp + 16]
+
     mov rsi, [rsp + 16]
     mov rdi, rsi
-    call str_to_int       ; => rax contient le nombre
-    mov rdi, rax          ; on met le nombre dans rdi
+    call str_to_int
+    mov rdi, rax
     call dec_to_hex
     jmp exit_success
 
-; -------------------------------------------------------------------
-; binary_convert:
-;   ./asm_convert -b 42
-;   => convertit 42 (argv[2]) en binaire
-; -------------------------------------------------------------------
 binary_convert:
-    ; On s’attend à ce que argv[1] == "-b"
-    ; argv[1] = [rsp + 16]
     mov rsi, [rsp + 16]
     cmp byte [rsi], '-'
     jne error_param
     cmp byte [rsi + 1], 'b'
     jne error_param
 
-    ; argv[2] = [rsp + 24]
     mov rsi, [rsp + 24]
     mov rdi, rsi
-    call str_to_int       ; => rax
+    call str_to_int
     mov rdi, rax
     call dec_to_bin
     jmp exit_success
 
-; -------------------------------------------------------------------
-; str_to_int
-;  Convertit la chaîne pointée par rdi en entier décimal (base 10).
-;  Retourne le résultat dans rax.
-; -------------------------------------------------------------------
 str_to_int:
     xor rax, rax
 .convert_loop:
     movzx rcx, byte [rdi]
     test rcx, rcx
-    jz .done            ; fin de chaîne => on sort
+    jz .done
     sub rcx, '0'
     imul rax, rax, 10
     add rax, rcx
@@ -78,23 +47,18 @@ str_to_int:
 .done:
     ret
 
-; -------------------------------------------------------------------
-; dec_to_hex
-;  Convertit le nombre (en rdi) en hexadécimal et l’affiche,
-;  suivi d’un saut de ligne.
-; -------------------------------------------------------------------
 dec_to_hex:
-    mov rax, rdi         ; rax = valeur à convertir
+    mov rax, rdi
     mov rsi, buffer
     add rsi, 19
-    mov byte [rsi], 0    ; terminaison de chaîne
+    mov byte [rsi], 0
 
     dec rsi
     mov rcx, 16
 
 .hex_loop:
     xor rdx, rdx
-    div rcx              ; rax / 16, reste dans rdx
+    div rcx
     cmp dl, 10
     jge .letter
     add dl, '0'
@@ -110,16 +74,14 @@ dec_to_hex:
 
     inc rsi
 
-    ; Appel système write(1, rsi, longueur)
-    mov rax, 1           ; syscall write
-    mov rdi, 1           ; fd=1 (stdout)
-    ; rsi pointe déjà sur le début de la chaîne
+    mov rax, 1
+    mov rdi, 1
+    
     mov rdx, buffer
-    add rdx, 20          ; rdx = fin buffer
-    sub rdx, rsi         ; longueur = (fin) - (début)
+    add rdx, 20
+    sub rdx, rsi
     syscall
 
-    ; Saut de ligne
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
@@ -127,11 +89,6 @@ dec_to_hex:
     syscall
     ret
 
-; -------------------------------------------------------------------
-; dec_to_bin
-;  Convertit le nombre (en rdi) en binaire et l’affiche,
-;  suivi d’un saut de ligne.
-; -------------------------------------------------------------------
 dec_to_bin:
     mov rax, rdi
     mov rsi, buffer
@@ -143,7 +100,7 @@ dec_to_bin:
 
 .bin_loop:
     xor rdx, rdx
-    div rcx              ; rax / 2, reste dans rdx
+    div rcx
     add dl, '0'
     mov [rsi], dl
     dec rsi
@@ -152,16 +109,14 @@ dec_to_bin:
 
     inc rsi
 
-    ; Appel système write(1, rsi, longueur)
-    mov rax, 1           ; syscall write
-    mov rdi, 1           ; fd=1 (stdout)
-    ; rsi = début de la chaîne convertie
+    mov rax, 1
+    mov rdi, 1
+
     mov rdx, buffer
     add rdx, 20
     sub rdx, rsi
     syscall
 
-    ; Saut de ligne
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
@@ -169,20 +124,14 @@ dec_to_bin:
     syscall
     ret
 
-; -------------------------------------------------------------------
-; error_param: quitter avec code 1
-; -------------------------------------------------------------------
 error_param:
-    mov rax, 60    ; syscall exit
-    mov rdi, 1     ; code de sortie = 1
+    mov rax, 60
+    mov rdi, 1
     syscall
 
-; -------------------------------------------------------------------
-; exit_success: quitter avec code 0
-; -------------------------------------------------------------------
 exit_success:
     mov rax, 60
-    xor rdi, rdi   ; rdi=0
+    xor rdi, rdi
     syscall
 
 section .data
