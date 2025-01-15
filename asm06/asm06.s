5cd4bfd5a537d3b9a3ae79eedf9229760281e6f3
Wany917 <yvannkakou03@gmail.com>
2025-01-15 08:40:46 +0100

    asm06 done

---
+++ asm06/asm06.s
@@ -0,0 +1,103 @@
+section .text
+global _start
+
+_start:
+    
+    pop rax       
+    pop rax      
+    pop rdi      
+    call str_to_int
+    mov r12, rax   
+    
+    pop rdi       
+    call str_to_int
+    
+ 
+    add rax, r12
+    
+    
+    mov rdi, rax
+    call display_number
+    
+    
+    mov rax, 1        
+    mov rdi, 1         
+    mov rsi, newline   
+    mov rdx, 1         
+    syscall
+    
+  
+    mov rax, 60        
+    xor rdi, rdi     
+    syscall
+
+
+str_to_int:
+    xor rax, rax       
+    xor rcx, rcx        
+    cmp byte [rdi], '-' 
+    jne .next_char
+    inc rdi            
+    mov rcx, 1         
+
+.next_char:
+    movzx rdx, byte [rdi]
+    test rdx, rdx
+    jz .done
+    sub rdx, '0' 
+    imul rax, 10     
+    add rax, rdx     
+    inc rdi         
+    jmp .next_char
+
+.done:
+    test rcx, rcx 
+    jz .finish
+    neg rax        
+.finish:
+    ret
+
+
+display_number:
+    mov rax, rdi
+    mov rsi, buffer
+    mov byte [rsi], ' '
+    inc rsi
+    
+    
+    test rax, rax
+    jns .convert
+    neg rax
+    mov byte [buffer], '-'
+
+.convert:
+    mov rcx, 10
+    mov rdi, buffer_end
+    mov byte [rdi], 0
+    dec rdi
+
+.convert_loop:
+    xor rdx, rdx
+    div rcx          
+    add dl, '0'         
+    mov [rdi], dl      
+    dec rdi             
+    test rax, rax      
+    jnz .convert_loop
+
+    lea rsi, [buffer]
+    mov rdx, buffer_end
+    sub rdx, rsi
+
+
+    mov rax, 1          
+    mov rdi, 1         
+    syscall
+    ret
+
+section .data
+    newline db 10
+
+section .bss
+    buffer resb 20
+    buffer_end equ buffer + 19 
