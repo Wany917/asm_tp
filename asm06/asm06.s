3568126dbfa728e351e8e8c5300e7bfabf4ba45e
Wany917 <yvannkakou03@gmail.com>
2025-01-15 08:44:55 +0100

    error gestion asm06

--- asm06/asm06.s
+++ asm06/asm06.s
@@ -2,68 +2,74 @@ section .text
 global _start
 
 _start:
-    
-    pop rax       
-    pop rax      
-    pop rdi      
+    pop rax
+    cmp rax, 3
+    jne error_param
+
+    pop rax
+    pop rdi
     call str_to_int
-    mov r12, rax   
+    mov r12, rax
     
-    pop rdi       
+    pop rdi
     call str_to_int
     
- 
     add rax, r12
-    
     
     mov rdi, rax
     call display_number
     
-    
-    mov rax, 1        
-    mov rdi, 1         
-    mov rsi, newline   
-    mov rdx, 1         
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
     syscall
     
-  
-    mov rax, 60        
-    xor rdi, rdi     
+    mov rax, 60
+    xor rdi, rdi
     syscall
 
+error_param:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, error_msg
+    mov rdx, error_len
+    syscall
+    
+    mov rax, 60
+    mov rdi, 1
+    syscall
 
 str_to_int:
-    xor rax, rax       
-    xor rcx, rcx        
-    cmp byte [rdi], '-' 
+    xor rax, rax
+    xor rcx, rcx
+    cmp byte [rdi], '-'
     jne .next_char
-    inc rdi            
-    mov rcx, 1         
+    inc rdi
+    mov rcx, 1
 
 .next_char:
     movzx rdx, byte [rdi]
     test rdx, rdx
     jz .done
-    sub rdx, '0' 
-    imul rax, 10     
-    add rax, rdx     
-    inc rdi         
+    sub rdx, '0'
+    imul rax, 10
+    add rax, rdx
+    inc rdi
     jmp .next_char
 
 .done:
-    test rcx, rcx 
+    test rcx, rcx
     jz .finish
-    neg rax        
+    neg rax
 .finish:
     ret
-
 
 display_number:
     mov rax, rdi
     mov rsi, buffer
     mov byte [rsi], ' '
     inc rsi
-    
     
     test rax, rax
     jns .convert
@@ -78,26 +84,27 @@ display_number:
 
 .convert_loop:
     xor rdx, rdx
-    div rcx          
-    add dl, '0'         
-    mov [rdi], dl      
-    dec rdi             
-    test rax, rax      
+    div rcx
+    add dl, '0'
+    mov [rdi], dl
+    dec rdi
+    test rax, rax
     jnz .convert_loop
 
     lea rsi, [buffer]
     mov rdx, buffer_end
     sub rdx, rsi
 
-
-    mov rax, 1          
-    mov rdi, 1         
+    mov rax, 1
+    mov rdi, 1
     syscall
     ret
 
 section .data
     newline db 10
+    error_msg db "Error: two parameters required", 10
+    error_len equ $ - error_msg
 
 section .bss
     buffer resb 20
-    buffer_end equ buffer + 19 
+    buffer_end equ buffer + 19
